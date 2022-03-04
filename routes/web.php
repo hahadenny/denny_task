63438bd91023e79c776492bf45e4381930745537
Denny Choi <dchoi@teamamericany.com>
2022-03-04 14:25:21 -0500

    home page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'TaskController@showTasks');
+
 Route::get('showTasks', 'TaskController@showTasks');
 
 Route::post('addTask', 'TaskController@addTask');
